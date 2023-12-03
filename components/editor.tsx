455f9f0b2d71ce949b262383bab7707803bc8519
yakshith <yakshith027@gmail.com>
2023-12-04 00:57:48 +0530

    course setup done and text editor added

---
+++ components/editor.tsx
@@ -0,0 +1,51 @@
+'use client'
+
+import dynamic from 'next/dynamic'
+import { useMemo } from 'react'
+
+import 'react-quill/dist/quill.snow.css'
+
+interface EditorProps {
+  onChange: (value: string) => void
+  value: string
+}
+
+export const Editor = ({ onChange, value }: EditorProps) => {
+  const ReactQuill = useMemo(
+    () => dynamic(() => import('react-quill'), { ssr: false }),
+    []
+  )
+  console.log('value', value)
+  return (
+    <div className='bg-white'>
+      <ReactQuill
+        theme='snow'
+        modules={{
+          toolbar: [
+            [{ header: '1' }, { header: '2' }, { font: [] }],
+            [{ size: [] }],
+            ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+            [
+              { list: 'ordered' },
+              { list: 'bullet' },
+              { indent: '-1' },
+              { indent: '+1' },
+            ],
+            ['link', 'image', 'video'],
+            [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+            [{ font: [] }],
+            [{ align: [] }],
+
+            ['clean'],
+          ],
+          clipboard: {
+            // toggle to add extra line breaks when pasting HTML:
+            matchVisual: false,
+          },
+        }}
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  )
+}
