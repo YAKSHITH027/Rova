aa8be2098c6b1d46ec5ad63cfc1cefd127cf51ef
yakshith <yakshith027@gmail.com>
2024-02-12 23:37:45 +0530

    route added

--- app/(group)/(dashboard)/_components/logo.tsx
+++ app/(group)/(dashboard)/_components/logo.tsx
@@ -1,8 +1,17 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 const Logo = () => {
   return (
-    <Image color='white' height={130} width={130} alt='logo' src='/logo.svg' />
+    <Link href='/'>
+      <Image
+        color='white'
+        height={130}
+        width={130}
+        alt='logo'
+        src='/logo.svg'
+      />
+    </Link>
   )
 }
 
